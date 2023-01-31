472e0582934d8dd8696fb8571507c09c1cdaa2dc
Rauf <raufmustafazade10@gmail.com>
2023-01-31 13:10:19 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,77 @@
+// task 1(input ile daxil olunan ededlerin cemi)
+// var a = +prompt('Ededi dahil edin')
+// let b = +prompt('Eded dahil edin')
+// let c 
+// c = a + b
+// console.log(c)
+
+// task 2(input ile daxil olunan musbet ededlerin cemi)
+// let a = +prompt(' musbet ededi dahil edin')
+// let b = +prompt(' musbet ededi dahil edin')
+// let c = +prompt(' musbet ededi dahil edin')
+// let eded = a + b + c
+// console.log(eded );
+ 
+// task 3(input ile daxil olunan ededlerin sifir,musbet yoxsa menfi oldugunu tapin)
+// let a = +prompt(' eded dahil edin')
+// if(a>0){
+//     console.log('eded musbetdi');
+// }
+// else if(a<0){
+//     console.log('eded menfidi');   
+// }
+// else if(a==0){
+//     console.log('eded sifirdi' );   
+// }
+// else{
+//     console.log('eded reqem deyil' );   
+// }
+
+// task 4(input ile daxil olunan 2 eded arasinda olan en boyuk ededi tapin)
+// var a = +prompt('Ededi dahil edin')
+// let b = +prompt('Eded dahil edin')
+// if(a>b){
+//     console.log('a boyukdu b-dan');
+// }
+//  else if(a<b){
+//     console.log('b boyukdu a-dan');
+// }
+// else if(a==b){
+//          console.log('a beraberdir b-ya' );
+// }
+// else{
+//         console.log('eded reqem deyil' );   
+// }
+
+// task 5(input ile daxil olunan ededin cut yoxsa tek oldugunu tapin)
+// var a = +prompt('Ededi dahil edin')
+// if(a%2==0){
+//     console.log('eded cemdi' );   
+// }
+// else if(a%2>0){
+//     console.log('eded tekdi' );   
+// }
+// else{
+//     console.log('eded reqem deyil' );   
+// }
+
+// task 7(input ile daxil olunan 3 eded arasinda olan en boyuk ededi tapin)
+// let a = +prompt('Ededi dahil edin')
+// let b = +prompt('Ededi dahil edinn')
+// let c = +prompt('Ededi dahil edinnn')
+// if(a>b>c){
+//     console.log('a ededi boyukdu ');
+// }
+// else if(b>a>c){
+//     console.log('b ededi boyukdu ');
+// }
+// else if(c>a>c) {
+//     console.log('c ededi boyukdu ');
+// }
+// else if(a==b==c){
+//     console.log('3 eded beraberdir ');
+// }
+// else{
+//     console.log('eded reqem deyil');   
+// }
+
